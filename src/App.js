5702ec18e1dca00721b652cceefa6782aa39e6ca
mica-oz <kahulaoz@gmail.com>
2023-08-30 21:03:09 -0600

    routes functional

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import Home from "./Home/Home.js";
 import About from "./About/About.js";
+import Gallery from "./Gallery/Gallery.js";
 
-import { BrowserRouter as Browser, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Home />
-        {/* <About /> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/gallery" element={<Gallery />} />
+        </Routes>
       </div>
     </Router>
   );
---
+++ src/Gallery/Gallery.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Gallery() {
+  return (
+    <div id="flex">
+      <h1>gallery</h1>
+    </div>
+  );
+}
+
+export default Gallery;
