68f9357c7fa25fc74649be8255c1bb059640ed93
mica-oz <kahulaoz@gmail.com>
2023-09-08 15:47:17 -0600

    about page round 1 complete

--- src/About/About.js
+++ src/About/About.js
@@ -1,42 +1,54 @@
 import React from "react";
 import headshot from "./mica-sq.jpeg";
 import "./About.css";
+import Checker1 from "./checker1.js";
+import { Link } from "react-router-dom";
+import home from "./left-arrow.svg";
 
 function About() {
   return (
     <div id="flex">
+      <div id="abtLogo">
+        <Link to="/">
+          <img src={home} alt="home" />
+        </Link>
+      </div>
       <div id="split">
         <div id="left">
-          <h1 id="about">ABOUT</h1>
+          <h1 id="about">MICA OZ</h1>
           <img src={headshot} alt="me" />
         </div>
         <div id="right">
           <h3 id="some">Some things you might be wondering</h3>
           <h3 id="me">about me:</h3>
           <div id="aboutContainer">
-            <h4> •I’m a Full Stack Software Engineer and Visual Artist.</h4>
-            <h4> •I’m currently located in Carbondale, Colorado.</h4>
+            <h4> + I’m a Full Stack Software Engineer and Visual Artist.</h4>
+            <h4> + I’m currently located in Carbondale, Colorado.</h4>
             <h4>
               {" "}
-              •My last name is Oszczakiewicz, pronounced osh-cha-kev-itch, but
+              + My last name is Oszczakiewicz, pronounced osh-cha-kev-itch, but
               please don’t hurt yourself. Oz for short is just fine.
             </h4>
             <h4>
               {" "}
-              •My mediums include, but are not limited to: code, digital
+              + My mediums include, but are not limited to: code, digital
               graphics, motion graphics, paint, animation, light, and
               environment.
             </h4>
             <h4>
               {" "}
-              •I periodically do freelance dev or design work for the right
+              + I periodically do freelance dev or design work for the right
               project. If you think we are a good fit, feel free to hit my{" "}
-              <button id="inlineButton">line</button>
+              <a href="mailto:mica.oszczakiewicz@gmail.com" id="inlineButton">
+                line.
+              </a>
             </h4>
           </div>
         </div>
       </div>
-      <div id="graphic"></div>
+      <div id="graphic">
+        <Checker1 />
+      </div>
     </div>
   );
 }
---
+++ src/About/checker1.js
@@ -0,0 +1,81 @@
+import React from "react";
+import "./checker1.css";
+import "./About.css";
+
+function Checker1() {
+  return (
+    <div id="container">
+      <div className="crow1">
+        <div className="s1"></div>
+        <div className="s2"></div>
+        <div className="s1"></div>
+        <div className="s2"></div>
+        <div className="s1"></div>
+        <div className="s2"></div>
+        <div className="s1"></div>
+        <div className="s2"></div>
+        <div className="s1"></div>
+        <div className="s2"></div>
+        <div className="s1"></div>
+        <div className="s2"></div>
+        <div className="s1"></div>
+        <div className="s2"></div>
+        <div className="s1"></div>
+        <div className="s2"></div>
+        <div className="s1"></div>
+        <div className="s2"></div>
+        <div className="s1"></div>
+        <div className="s2"></div>
+        <div className="s1"></div>
+      </div>
+      <div className="crow1">
+        <div className="s2"></div>
+        <div className="s1"></div>
+        <div className="s2"></div>
+        <div className="s1"></div>
+        <div className="s2"></div>
+        <div className="s1"></div>
+        <div className="s2"></div>
+        <div className="s1"></div>
+        <div className="s2"></div>
+        <div className="s1"></div>
+        <div className="s2"></div>
+        <div className="s1"></div>
+        <div className="s2"></div>
+        <div className="s1"></div>
+        <div className="s2"></div>
+        <div className="s1"></div>
+        <div className="s2"></div>
+        <div className="s1"></div>
+        <div className="s2"></div>
+        <div className="s1"></div>
+        <div className="s2"></div>
+      </div>
+      <div className="crow1">
+        <div className="s1"></div>
+        <div className="s2"></div>
+        <div className="s1"></div>
+        <div className="s2"></div>
+        <div className="s1"></div>
+        <div className="s2"></div>
+        <div className="s1"></div>
+        <div className="s2"></div>
+        <div className="s1"></div>
+        <div className="s2"></div>
+        <div className="s1"></div>
+        <div className="s2"></div>
+        <div className="s1"></div>
+        <div className="s2"></div>
+        <div className="s1"></div>
+        <div className="s2"></div>
+        <div className="s1"></div>
+        <div className="s2"></div>
+        <div className="s1"></div>
+        <div className="s2"></div>
+        <div className="s1"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Checker1;
