8ad6dd6e6e0a75127420abc1169d3e0ebf1af856
mica-oz <kahulaoz@gmail.com>
2023-09-10 16:03:54 -0600

    single project formatted

--- src/SWE/Projects/rotoflo.js
+++ src/SWE/Projects/rotoflo.js
@@ -1,39 +1,42 @@
 import React from "react";
 import "./rotoflo.css";
-import PropTypes from "prop-types";
 
 function Rotoflo() {
   return (
     <div id="rotofloFlex">
-      <div id="header">
-        <h3>Title: ROTOFLO</h3>
+      <div id="projHeader">
+        <h3>TITLE: ROTOFLO</h3>
         <h3>
-          Link: <a href="www.rotoflo.herokuapp.com">ROTOFLO</a>
-        </h3>
-        <h3>
-          Description: A motion interactive AR experience. Rotoflo leverages
+          DESCRIPTION: A motion interactive AR experience. Rotoflo leverages
           machine learning to generate dynamic, algorithmic, full body filters,
           allowing users to save and share stunning and expressive video
           content.{" "}
         </h3>
         <h3>
-          Press:{" "}
+          SITE: <a href="www.rotoflo.herokuapp.com">ROTOFLO</a>
+        </h3>
+        <h3>
+          REPO: <a href="https://github.com/GH-Team-Coconut/RotoFlo">GITHUB</a>
+        </h3>
+
+        <h3>
+          PRESS:{" "}
           <a href="https://www.youtube.com/watch?v=ijz9nGKCrkI&t=2s">
-            Google - Tensorflow.js Interview
+            GOOGLE - TENSORFLOW.JS INTERVIEW
           </a>
         </h3>
       </div>
       <div id="body">
-        <div id="prpjectPlayer">
+        <div id="projectPlayer">
           <iframe
-            width="853"
-            height="480"
-            src={`https://www.youtube.com/watch?v=A9LnAh6N1rY`}
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-            title="Embedded youtube"
-          />
+            // width="560"
+            // height="315"
+            src="https://www.youtube.com/embed/A9LnAh6N1rY?si=p1Q6DyneHPA4LhfY"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
         </div>
       </div>
     </div>
